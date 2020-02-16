824f9cc13f82e8244c6f0ac25b2093b9cb3534e7
Browning Smith <browningsmith@email.arizona.edu>
2020-02-16 13:05:01 -0700

    able to successfully animate construction of UCH

--- convexhull.js
+++ convexhull.js
@@ -15,7 +15,7 @@ var hullIterator = null;
 var upperHull = null;
 
 //Flag to say whether we are checking upper hull, or adding a new Point
-var checkingUpperHull = false;
+var checkingUpperHull = true;
 
 //Flag to say whether we are animating convex hull or not
 var animating = false;
@@ -104,6 +104,9 @@ function addPoint(event) {
         return;
 	}
 
+    //Change instructions
+    document.getElementById("instructions").innerHTML = "Now press any key on the keyboard to watch the convex hull be constructed.";
+
 	//Set animating to true
 	animating = true;
 
@@ -134,20 +137,40 @@ function animateHull(event) {
 	}
 
     //Draw all points
+    console.log("Drawing points");
     drawAllPoints();
 
     //Check to see if we are checking to reject points, or adding a new point.
     if (checkingUpperHull) {
     
-        
+        console.log("Checking flag is set to true. Checking points to reject on upper hull");
+        //Check the upper hull for bad Points
+        checkUpperHull();
 	}
     else {
     
         //Add a new point to the upperHull
-        addToUpperHull();
+        console.log("Checking flag is set to false. Adding new point to upper hull. Setting checking flag to true");
+
+        //If a new point is added to the upperHull, set checkingUpperHull to true
+        if (addToUpperHull()) {
+      
+            checkingUpperHull = true;
+		}
+        else {
+      
+            //otherwise set checkingUpperHull to false and set animating to false
+
+            document.getElementById("instructions").innerHTML = "Upper Hull completed. Click to add a new point.";
+            console.log("No more points to add to upper hull. Setting checking to false and animating to false");
+            checkingUpperHull = false;
+            animating = false;
+		}
 	}
 
     //Draw the hull
+
+    console.log("Drawing lines of upper hull");
     drawHull(upperHull);
 }
 
@@ -159,15 +182,75 @@ function addToUpperHull() {
     //if newPointToAdd is null, set animating to false and return
     if (newPointToAdd === null) {
     
-        console.log("Upper Hull computed");
-        animating = false;
-        return;
+        console.log("No more points to add");
+        return false; //return false if unable to add more points
 	}
 
     //Push new point on to the linked list queue
     var oldUpperHull = upperHull; //Grab the front of the queue
     upperHull = new LinkedNode(newPointToAdd); //Create a new linked list node containing the BST node
     upperHull.setNext(oldUpperHull); //Attach the old list
+
+    return true; //Return true since a new point was added
+}
+
+//Method to check last three points in upperHull queue, and reject if necessary
+function checkUpperHull() {
+
+    //If the hull is empty, set checkingUpperHull to false and return
+    if (upperHull === null) {
+    
+        console.log("Upper hull is empty, finishing check, setting checking flag to false");
+        checkingUpperHull = false;
+        return;
+	}
+
+    //Variable to hold point3
+    console.log("Recording point3");
+    var point3 = upperHull;
+
+    //If point3 has no neighboring point2, set checkingUpperHull to false and return
+    if (point3.nextExists() == false) {
+    
+        console.log("Upper hull has less than two points. Finishing check, setting checking flag to false");
+        checkingUpperHull = false;
+        return;
+	}
+
+    //Variable to hold point2
+    console.log("Recording point2");
+    var point2 = point3.getNext();
+
+    //If point2 has no neighboring point1, set checkingUpperHull to false and return
+    if (point2.nextExists() == false) {
+    
+        console.log("Upper hull has less than three points. Finishing check, setting checking flag to false");
+        checkingUpperHull = false;
+        return;
+	}
+
+    //variable to hold point1
+    console.log("Recording point1");
+    var point1 = point2.getNext();
+
+    //Compute the orientation
+    console.log("Computing orientation");
+    var orientation = ((point1.getContent().getContent().getY() - point2.getContent().getContent().getY()) * (point3.getContent().getContent().getX() - point2.getContent().getContent().getX())) - ((point2.getContent().getContent().getY() - point3.getContent().getContent().getY()) * (point2.getContent().getContent().getX() - point1.getContent().getContent().getX()));
+
+    //If the orientation is negative (counterclockwise) reject point2
+    if (orientation < 0) {
+    
+        console.log("Orientation determined to be counterclockwise. Rejecting point2. Checking flag should remain true.");
+        //Attach point1 as point3's next element
+        point3.setNext(point1);
+	}
+
+    //If the orientation is positive or colinear, accept point2 and proceed
+    else {
+    
+        console.log("Orientation determined to be clockwise. Keeping point2. Finishing check, setting checking flag to false");
+        checkingUpperHull = false;
+	}
 }
 
 //Method to reset main BST iterator
